88134af27aa4cae03743022910b79084abce3f46
xssokolova <48553308+xssokolova@users.noreply.github.com>
2020-04-16 23:01:32 +0300

    Update server.R

--- server.R
+++ server.R
@@ -1,22 +1,36 @@
 library(shiny)
+library(UsingR)
+data(diamond)
+
 shinyServer(function(input, output) {
-
-
+    
+    model <- lm(price ~ carat, data = diamond)
+    
+    
     modelpred <- reactive({
-        mpgInput <- input$sliderMPG
-
+        CaratInput <- input$sliderCarat
+        predict(model, newdata = data.frame(carat = CaratInput))
     })
-
-
+    
+    
     output$plot <- renderPlot({
-        mpgInput <- input$sliderMPG
-
-        boxplot(mpg ~ am, data = mtcars,
-                main = "Plot: Fuel flow by Transmission type",
-                xlab = "Transmission type (0 - auto,1 - manual)",
-                ylab = "Fuel flow (mpg)")
-        abline(h=mpgInput, col= "red")
+        CaratInput <- input$sliderCarat
+        
+        plot(diamond$carat, diamond$price, xlab = "Mass (carats)", 
+             ylab = "Price (SIN $)", bty = "n", pch = 16,
+             xlim = c(0, 6), ylim = c(0, 20000))
+        if(input$showModel){
+            abline(model, col = "red", lwd = 2)
+        }
+        points(CaratInput, modelpred(), col = "red", pch = 16, cex = 2)
+        
+        
     })
-
+    
+    output$pred <- renderText({
+        modelpred()
+    })
+    
 
 })
+        
